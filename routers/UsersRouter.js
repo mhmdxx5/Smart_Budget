5dd04b6d2d3625afbac8b9b204e190f43ef0c8b9
idankario <idankario@gmail.com>
2021-12-23 12:32:12 +0200

    orginizefile

--- routers/UsersRouter.js
+++ routers/UsersRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const{UsersController}=require('../controllers/UsersController')
+const { UsersController } = require('../controllers/UsersController')
 const UsersRouter = new Router();
 UsersRouter.get('/', UsersController.getUsers);
 UsersRouter.get('/:id', UsersController.getUser);
-UsersRouter.post('/', UsersController.postUser); 
-UsersRouter.post('/family', UsersController.addfamily); 
-UsersRouter.put('/:id', UsersController.updateUser); 
+UsersRouter.post('/', UsersController.postUser);
+UsersRouter.post('/family', UsersController.addfamily);
+UsersRouter.put('/:id', UsersController.updateUser);
 UsersRouter.delete('/:id', UsersController.deleteUser);
-module.exports = {UsersRouter};
+module.exports = { UsersRouter };
